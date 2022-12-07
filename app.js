754419f5cf37713deb2814ed0cae30f4f5acd9f8
Thorjuri <sjl1992@naver.com>
2022-12-07 23:12:30 +0900

    12/7 jest 스크립트 변경, 강제종료 및 비동기  추적5. unittest 1차시도 성공, 2차시도

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(errorHandlerMiddleware);
 
 app.get("/", (req, res) => {
-  res.send("SPOTS 서버 상태 양호😏");
+  res.send("SPOTS 서버 상태 양호😏😏");
 });
 
 socket(http);
